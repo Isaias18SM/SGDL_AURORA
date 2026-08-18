9dd39487da7aa1cd224624a9435ffb6ccbd3c0d4
henrry <alexvelandia618@gmail.com>
2026-08-17 23:06:28 -0500

    interfaz de intructor modificada y arreglada la funcionalidad de los distintos botenes que lo componen

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from routes_instructor import instructor_bp
 from routes_aprendiz import aprendiz_bp
 from routes_api import api_bp
 from app.routes.coordinador import coordinador
+from app.routes.fichas import fichas_bp
 
 app = Flask(__name__, static_folder='app/static')
 app.secret_key = 'aurora_sena_secret_2026'
@@ -13,5 +14,8 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(instructor_bp)
 app.register_blueprint(aprendiz_bp)
 app.register_blueprint(coordinador)
+app.register_blueprint(fichas_bp)
+app.register_blueprint(api_bp)  # <- faltaba esta línea, por eso /api/fichas y /api/buscar-aprendiz daban 404
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
